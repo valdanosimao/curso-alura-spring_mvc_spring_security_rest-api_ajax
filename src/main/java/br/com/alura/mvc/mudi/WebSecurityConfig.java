2bdbccf50d3be00e4479d1ece6edc8204f791209
Valdano Simão <valdanosimao@gmail.com>
2022-05-12 18:37:27 -0300

    usando jdbc authentication

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,44 +1,48 @@
 package br.com.alura.mvc.mudi;
 
-import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	private final DataSource dataSource;
+
+	public WebSecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
 			.anyRequest().authenticated()
 		.and()
-			.formLogin(form -> form
-					.loginPage("/login")
-					.permitAll()
-					
-					)
-			.logout(logout -> logout.logoutUrl("/logout"));
+		.formLogin(form -> form
+            .loginPage("/login")
+            .defaultSuccessUrl("/home", true)
+            .permitAll()
+        )
+		.logout(logout -> logout.logoutUrl("/logout"));
 	}
-	 
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-				User.withDefaultPasswordEncoder()
-				.username("valdano")
-				.password("3213")
-				.roles("ADM")
-				.build();
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			
 		
-		return new InMemoryUserDetailsManager(user);
+		
+			auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
 	}
+	
 	
 }
